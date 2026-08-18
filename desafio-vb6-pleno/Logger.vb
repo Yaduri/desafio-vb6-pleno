6f52b8d29175e44cf9383fb8f6c42a4c8b094a03
Yago Duarte <yaduri16@gmail.com>
2026-08-18 16:32:47 -0300

    Atualização logger, mostrar caminho do arquivo

--- desafio-vb6-pleno/Logger.vb
+++ desafio-vb6-pleno/Logger.vb
@@ -2,7 +2,11 @@
 
 Public Module Logger
     Private ReadOnly LogDirectory As String = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs")
-    Private ReadOnly LogPath As String = Path.Combine(LogDirectory, $"log_{DateTime.Now:yyyyMMdd}.txt")
+    Public ReadOnly Property LogFilePath As String
+        Get
+            Return Path.Combine(LogDirectory, $"log_{DateTime.Now:yyyyMMdd}.txt")
+        End Get
+    End Property
 
     Public Sub LogError(ex As Exception, Optional contextualInfo As String = "")
         Try
@@ -10,7 +14,7 @@ Public Module Logger
                 Directory.CreateDirectory(LogDirectory)
             End If
 
-            Using writer As New StreamWriter(LogPath, True)
+            Using writer As New StreamWriter(LogFilePath, True)
                 writer.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] [ERRO] {contextualInfo}")
                 writer.WriteLine($"Mensagem: {ex.Message}")
                 writer.WriteLine($"StackTrace: {ex.StackTrace}")
